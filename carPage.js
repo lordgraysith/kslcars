07db4e2f4f202e9fd13a2871fcdfc7b400259e7d
Michael Graybeal <michael@thegraybeals.com>
2013-04-08 01:42:40 -0600

    added data miner for car page

--- carPage.js
+++ carPage.js
@@ -4,16 +4,64 @@ var DataMiner = (function(){
 	, model
 	, url
 	, phoneNumber
-	, year;
+	, year
+	, mileage
+	, adID
+	, specs
+	, getSpec;
+
+	getSpec = function(specName){
+		var iter
+		, row
+		, rowName
+		, rowValue
+		, result = 'Not Specified';
+
+		for(iter = 0; iter < specs.children.length; iter++){
+			row = specs.children[iter];
+			rowName = row.children[0].textContent.replace(':','');
+			rowValue = row.children[1].textContent;
+			if(specName === rowName){
+				result = rowValue;
+				break;
+			}
+		}
+
+		return result;
+	};
+
+	specs = document.getElementById('specificationsTable').children[0];
+
+	//set price
+	(function(){
+		var element = document.getElementsByClassName('price')[0];
+		price = element.textContent;
+	}());
+
+	//set phone number
+	(function(){
+		var phLink = document.getElementsByClassName('contactPhone')[0].children[0];
+		phoneNumber = phLink.childNodes[4].data.trim();
+	}());
+
+	url = document.location.href;
+	adID = document.getElementById('ad_id').textContent;
+
+	make = getSpec('Make');
+	year = getSpec('Year');
+	model = getSpec('Model');
+	mileage = getSpec('Mileage');
 
 	return{
 		carDetails: {
-			price = price
-			, model = model
-			, make = make
-			, url = url
-			, phoneNumber = phoneNumber
-			, year = year
+			price: price
+			, model: model
+			, make: make
+			, url: url
+			, phoneNumber: phoneNumber
+			, year: year
+			, mileage: mileage
+			, adID: adID
 		}
-	}
+	};
 }());
