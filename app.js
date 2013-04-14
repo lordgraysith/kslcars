b074120db47ba9e73a9dd95646e246b5be1e40ba
Michael Graybeal <michael@thegraybeals.com>
2013-04-14 16:17:21 -0600

    changed the main module to app.js

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-var express = require('express')
-, eventManager = require('./eventManager').getEventManager()
-, router = require('./routes').getRouter(eventManager)
+var eventManager
+, iter
+, express = require('express')
 , app
-, port = process.env.PORT || 5000;
+, port = 5000;
+
+//setup global variables
+global.mongolabURI = process.env.MONGOLAB_URI || 'mongodb://localhost/kslcars';
+global.kslStart = process.env.KSL_START || 'http://www.ksl.com/auto/search/index';
+
+eventManager = require('./eventManager').getEventManager();
+
+// for(iter = 3548; iter > -1; iter--){
+// 	eventManager.emit('sentinel:addListPage', global.kslStart + '?page=' + iter);
+// }
+
+//eventManager.emit('sentinel:addCarPage', 476005);
+//eventManager.emit('sentinel:addListPage', global.kslStart);
+//eventManager.emit('sentinel:start');
 
 app = express.createServer(express.logger());
 
 app.configure(function(){
   app.use(express.cookieParser());
   app.use(express.bodyParser());
-  app.use(express.session({ secret: process.env.SECRET || 'totally secure' }));
+  app.use(express.favicon());
 });
 
-app.get('/', router.root);
+app.get('/stop', function(req, res){
+	eventManager.emit('sentinel:stop');
+	res.send('stopped');
+});
+
+app.get('/start', function(req, res){
+	eventManager.emit('sentinel:start');
+	res.send('started');
+});
 
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
-
-//setup global variables
-global.mongolabURI = process.env.MONGOLAB_URI || 'mongodb://localhost/kslcars';
